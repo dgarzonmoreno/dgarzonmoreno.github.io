115ad5136df30fc3d6bb7937014cff484f57830c
dgarzonmoreno <dgarzonmoreno@ufl.edu>
2019-08-10 22:11:25 -0400

    updated react-app

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheets/index.scss';
+
+const App = () => {
+    return(
+        <p 
+        className = "appFont">
+            CRAP
+        </p>
+   );
+}
+
+export default App;
